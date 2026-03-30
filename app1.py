3c8d3f34b8990f01ffb7ae93acc6547461daa867
salomeriverod-hue <salomeriverod@gmail.com>
2026-03-29 21:01:44 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,9 +5,9 @@ import pytesseract
 from PIL import Image
 
 
-st.title("Reconocimiento óptico de Caracteres")
+st.title("Reconocimiento de identidad)
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.camera_input("Tómate una foto junto con tu documento de identificación")
 
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
